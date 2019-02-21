ba8157ac2ed67bbef0eef60b5bdf6f56559a3b94
Derek Maffett <derekgmaffett@gmail.com>
2019-02-21 18:36:23 +0100

    replace native query implementation with haskeline

--- src/Logger.hs
+++ src/Logger.hs
@@ -4,6 +4,8 @@ import           Config
 import           System.Exit
 import           System.Log.Logger
 import           System.IO
+import           System.Console.Haskeline
+import           Data.List                      ( isPrefixOf )
 import qualified Control.Monad.Reader          as Reader
 
 
@@ -23,7 +25,22 @@ logError msg = do
     Reader.liftIO $ criticalM logger msg
     Reader.liftIO $ exitFailure
 
--- Haskell prints only when a line is complete or the buffer is full.
--- Getting same-line input requires flushing the buffer early.
+-----------------------------------
+
 query :: String -> Program String
-query question = Reader.liftIO $ putStr question >> hFlush stdout >> getLine
+query question = Reader.liftIO $ runInputT defaultSettings $ do
+    maybeInput <- getInputLine question
+    case maybeInput of
+        Nothing    -> return ""
+        Just input -> return input
+
+queryWithSuggestions :: String -> [String] -> Program String
+queryWithSuggestions question wordList =
+    Reader.liftIO $ runInputT (setComplete completionFn defaultSettings) $ do
+        maybeInput <- getInputLine question
+        case maybeInput of
+            Nothing    -> return ""
+            Just input -> return input
+  where
+    searchFn str = simpleCompletion <$> filter (str `isPrefixOf`) wordList
+    completionFn = completeWord Nothing " \t" $ return . searchFn
