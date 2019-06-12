a60f627d17e54d6b8fded698b005ab80927d48b4
Derek Maffett <derekgmaffett@gmail.com>
2019-06-13 01:27:34 +0200

    bump npm package

--- bin/npm-postinstall.js
+++ bin/npm-postinstall.js
@@ -6,12 +6,12 @@ const os = require("os");
 const platform = os.platform();
 
 if (platform === "linux") {
-    fs.renameSync("./bin/s3/linux/river", "./bin/river");
+    fs.renameSync("./bin/river-linux", "./bin/river");
 } else if (platform === "darwin") {
-    fs.renameSync("./bin/s3/osx/river", "./bin/river");
+    fs.renameSync("./bin/river-osx", "./bin/river");
 } else {
     console.log(
         "OS not recognized or explicitly supported. Installing Linux binaries..."
     );
-    fs.renameSync("./bin/s3/linux/river", "./bin/river");
+    fs.renameSync("./bin/river-linux", "./bin/river");
 }
