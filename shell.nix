680d2ebfea6080dc3bd5210e176222204db8d882
Derek Maffett <derekgmaffett@gmail.com>
2019-09-17 17:41:58 +0200

    go back to using hpack

--- shell.nix
+++ shell.nix
@@ -8,11 +8,13 @@ pkgs.mkShell {
     app.shells.ghc
   ];
   buildInputs=[
+    pkgs.haskellPackages.hpack
     (pkgs.writeWatchScript {
       name = "watch-dev";
       src = "./app";
       command = "cabal new-build all";
     })
-    (pkgs.localCabalRun "river")
+    (pkgs.localCabalRun "river" "exe:river")
+    (pkgs.writeShellScriptBin "hpack-river" "cd ./app && hpack && cd ..")
   ];
 }
